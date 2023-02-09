1cf09b1895d8c134e943985ccab798ac071dfddc
Willian Pinho <kps_wpinho@uolinc.com>
2023-02-09 14:37:17 -0300

    Initial Commit

---
+++ Cats Everywhere/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cats Everywhere
+//
+//  Created by Willian Junior Peres de Pinho on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cats EverywhereTests/Cats_EverywhereTests.swift
@@ -0,0 +1,33 @@
+//
+//  Cats_EverywhereTests.swift
+//  Cats EverywhereTests
+//
+//  Created by Willian Junior Peres de Pinho on 09/02/23.
+//
+
+import XCTest
+@testable import Cats_Everywhere
+
+class Cats_EverywhereTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
